33b4f6c5dd511788d1f4dc7e06191759dc91e8ee
hugoxxdev <huguitoynoa@gmail.com>
2026-05-09 14:41:33 +0200

    fix: actualizar WEBSITE_URL a railway

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -30,7 +30,7 @@ export const APP_CONFIG = {
   // Redes sociales / enlaces
   GITHUB_URL: 'https://github.com/hugoxx-official/alfazulu',
   TELEGRAM_URL: 'https://t.me/alfazulu',
-  WEBSITE_URL: 'https://alfazulu.com',
+  WEBSITE_URL: 'https://alfazulu-production.up.railway.app',
 };
 
 // Features que se muestran en la landing
